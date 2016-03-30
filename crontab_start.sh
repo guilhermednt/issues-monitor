428abde2fc0ed538dad6afa48e61cdfdef660225
Guilherme Donato <guilhermednt@users.noreply.github.com>
2016-03-30 11:44:36 -0300

    better logging

--- crontab_start.sh
+++ crontab_start.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RESULT=`SCREEN=:0 DISPLAY=:0 XAUTHORITY=$HOME/.Xauthority $DIR/issues_monitor.py -c $DIR/config.ini 2>&1`
 
 if [[ $RESULT != *"Another instance is already running, quitting."* ]]; then
-	echo $RESULT >>$DIR/issues_monitor.log
+	printf "$RESULT \n\n" >>$DIR/issues_monitor.log
 fi
